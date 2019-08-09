abe4b5091f01bbe9c1c7f7cc09237558dd682ac9
Phi <git@proffitt.io>
2019-08-09 16:18:15 -0500

    added players to instance

--- src/instance.ts
+++ src/instance.ts
@@ -1,17 +1,25 @@
 import uuid = require('uuid/v4');
+import { Player } from './player';
 
 export class Instance {
     width: number;
     height: number;
+    players: Player[];
     constructor(public id: string, public attributes: InstanceAttributes) {
         this.width = attributes.width;
         this.height = attributes.height;
+        this.players = [];
+    }
+    updateAllPlayers(){
+        for (var plr of this.players){
+            plr.pushUpdate();
+        }
     }
     static generateNewInstanceID() {
         return uuid();
     }
     static generateRandomInstance() {
-        var attr:InstanceAttributes = new InstanceAttributes(4/* chosen by fair dice roll. guaranteed to be random */, 10, 10);
+        var attr: InstanceAttributes = new InstanceAttributes(4/* chosen by fair dice roll. guaranteed to be random */, 10, 10);
         return new Instance(this.generateNewInstanceID(), attr);
     }
 }
