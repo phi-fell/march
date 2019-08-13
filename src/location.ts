886979be0b130817e263ce7ddc139dfdd553a4c1
Phi <git@proffitt.io>
2019-08-13 16:20:41 -0500

    added clone function to location

--- src/location.ts
+++ src/location.ts
@@ -4,4 +4,7 @@ export class Location {
     getMovedBy(dx: number, dy: number) {
         return new Location(this.x + dx, this.y + dy, this.instance_id);
     }
+    clone() {
+        return new Location(this.x, this.y, this.instance_id);
+    }
 }
