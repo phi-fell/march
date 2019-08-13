b8e18a82a5b69965dac87b84df61c3a46907546b
Phi <git@proffitt.io>
2019-08-13 16:21:22 -0500

    entity default params and protected methods

--- src/entity.ts
+++ src/entity.ts
@@ -1,18 +1,27 @@
 import uuid = require('uuid/v4');
 import { Location } from './location';
+import { Instance } from './instance';
+import { Player } from './player';
 
 function generateNewEntityID() {
     return uuid();
 }
 export class Entity {
     health: number;
-    constructor(public id: string, public name: string, public location: Location) {
-        this.health = 10;
+    constructor(public id: string, public name: string, public location: Location = new Location(0, 0, '')) {
+        this.health = 2;
     }
-    private handleDeath() {
+    protected handleDeath() {
+        //TODO: add ability to attach listeners to entities e.g. death listener, damage listener, etc.
         //TODO: kill entity
+        Instance.removeEntityFromWorld(this);
+        if (this instanceof Player) {
+            (this as Player).user!.socket.emit('force_disconnect', 'YOU HAVE DIED');
+            (this as Player).user!.logout();
+            (this as Player).user!.unload();
+        }
     }
-    private takeDirectHealthDamage(amount) {
+    protected takeDirectHealthDamage(amount) {
         //take this damage directly to health and then account for effects (e.g. dying if health = 0 or whatever)
         //this functions is basically just health -= amount
         this.health -= amount;
@@ -20,7 +29,7 @@ export class Entity {
             this.handleDeath();
         }
     }
-    private takeNetDamage(amount) {
+    protected takeNetDamage(amount) {
         //apply this damage without accounting for armor, resistances, etc.
         //this function handles e.g. applying the damage first to a magical energy shield before actual health, or whatnot
         var shield = 0;//for example purposes. (should probably add a 'takeDirectShieldDamage' function if this were a feature)
@@ -32,7 +41,7 @@ export class Entity {
             this.takeDirectHealthDamage(netAmount);
         }
     }
-    private takeDamage(amount) {
+    protected takeDamage(amount) {
         //take amount damage, filtered through armor, resists, etc.
         var armor = 0;//for example purposes
         this.takeNetDamage(amount - armor);
@@ -43,5 +52,8 @@ export class Entity {
         if (Math.random() >= dodgeChance) {
             this.takeDamage(amount);
         }
+    }
+    static generateNewEntityID() {
+        return uuid();
     }
 }
