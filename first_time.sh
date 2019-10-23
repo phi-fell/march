838c92e4e9e54f6defde411faff904ec9256fd18
Phi <git@proffitt.io>
2019-10-22 20:04:22 -0500

    mkdirs is now first_time

--- first_time.sh
+++ first_time.sh
@@ -9,3 +9,7 @@ mkdir admin
 echo please enter an admin token: 
 read token
 printf $token > admin/token
+npm install
+tmux new -d -s node_march
+tmux send-keys -t node_march.0 "npm run prod" ENTER
+tmux a -t node_march
