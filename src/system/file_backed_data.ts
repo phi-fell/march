96da3a619c81944530570ee6a752ee8fbaad5457
Phi <git@proffitt.io>
2020-02-15 17:15:39 -0600

    FileBackData.ready()

--- src/system/file_backed_data.ts
+++ src/system/file_backed_data.ts
@@ -1,8 +1,15 @@
 import { OwnedFile } from './file';
 
 export abstract class FileBackedData {
+    private constructionPromise?: Promise<any>;
     protected constructor(private file: OwnedFile) {
-        this.fromJSON(file.getJSON());
+        this.constructionPromise = this.fromJSON(file.getJSON());
+    }
+    public async ready() {
+        if (this.constructionPromise) {
+            await this.constructionPromise;
+            this.constructionPromise = undefined;
+        }
     }
     public save() {
         this.file.setJSON(this.toJSON());
@@ -11,7 +18,7 @@ export abstract class FileBackedData {
     public async unload() {
         return Promise.all([this.file.release(), this.cleanup()]);
     }
-    protected abstract fromJSON(json: any): void;
+    protected abstract async fromJSON(json: any): Promise<void>;
     protected abstract toJSON(): any;
     /** does any necessary cleanup.  called by unload(), override in subclass if cleanup is needed */
     protected abstract async cleanup(): Promise<void>;
