ce1f9c4d6c4a6b0035646c901c37c36910e85359
phi <git@proffitt.io>
2020-04-09 14:29:09 -0500

    instance, method stubs

--- src/world/instance.ts
+++ src/world/instance.ts
@@ -1,5 +1,28 @@
-export class Instance {
-    public static fromJSON(json: any): any {
-        return;
+import { Random } from '../math/random';
+import type { OwnedFile } from '../system/file';
+import { FileBackedData } from '../system/file_backed_data';
+import type { Cell } from './cell';
+
+export class Instance extends FileBackedData {
+    public static generateNewID() {
+        return Random.uuid();
+    }
+    public static async loadInstanceFromFile(file: OwnedFile) {
+        const inst = new Instance(file);
+        await inst.ready();
+        return inst;
+    }
+    private cells: Cell[] = [];
+    protected constructor(file: OwnedFile, public id: string = Instance.generateNewID()) {
+        super(file);
+    }
+    public get schema(): import('io-ts').Any {
+        throw new Error('Method not implemented.');
+    }
+    protected async fromJSON(json: unknown): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    protected toJSON(): unknown {
+        throw new Error('Method not implemented.');
     }
 }
