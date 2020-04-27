6f81150c490b067e31e5c9e6f586bdd81838f69d
phi <git@proffitt.io>
2020-04-27 15:52:54 -0500

    Constructed<T>

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,2 +1,3 @@
 export type ValueOf<T> = T[keyof T];
 export type ValueOfArray<T extends { [index: number]: any }> = T[number];
+export type Constructed<T> = T extends new (...args: any) => infer U ? U : never
