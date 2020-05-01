75c536fb8219ca947dda143876ce4519b0f70e2e
phi <git@proffitt.io>
2020-05-01 13:02:51 -0500

    fix setup_prod.sh

--- setup_prod.sh
+++ setup_prod.sh
@@ -8,7 +8,7 @@ echo "{\"hash\":\"master\",\"rebuild\":false}" > config/launch.json
 mkdir admin
 echo please enter an admin token: 
 read token
-printf $token > admin/token
+printf "$token" > admin/token
 
 echo please enter an SSL certificate root directory:
 read ssl_root
@@ -16,8 +16,11 @@ echo please enter an SSL private key filename \(optional\):
 read ssl_key
 echo please enter an SSL certificate filename \(optional\):
 read ssl_cert
-json='{"root": "'"${ssl_root:-/etc/letsencrypt/live/gotg.io/}"'", "key": "'"${ssl_key:-privkey.pem}"'", "cert": "'"${ssl_cert:-cert.pem}"'"}'
-printf $json > config/https.json
+default_root="/etc/letsencrypt/live/gotg.io/"
+default_key="privkey.pem"
+default_cert="cert.pem"
+json='{"root": "'"${ssl_root:-$default_root}"'", "key": "'"${ssl_key:-$default_key}"'", "cert": "'"${ssl_cert:-$default_cert}"'"}'
+printf "$json" > config/https.json
 
 npm install
 tmux new -d -s node_march
