94fd0a5d6c41bc6525255685589b133d3dbdd27c
phi <git@proffitt.io>
2020-05-02 11:08:42 -0500

    fix cookies on itch.io

--- site/ts/auth.ts
+++ site/ts/auth.ts
@@ -4,6 +4,9 @@ export function setCookie(name: string, value: string, days: number) {
         const date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = '; expires=' + date.toUTCString();
+        if (window.location.hostname !== 'localhost') {
+            expires += ';SameSite=None;Secure'
+        }
     }
     document.cookie = name + '=' + (value || '') + expires + '; path=/';
 }
@@ -21,7 +24,7 @@ export function getCookie(name: string) {
     return null;
 }
 export function deleteCookie(name: string) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
 }
 export function cacheCredentials(creds: Credentials) {
     setCookie('user', creds.user || '', 3);
