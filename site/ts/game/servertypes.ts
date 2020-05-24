ad4039082d552c88f79ccf220788b17a1929d81f
phi <git@proffitt.io>
2020-05-24 13:07:22 -0500

    added character sheet server types

--- site/ts/game/servertypes.ts
+++ site/ts/game/servertypes.ts
@@ -17,11 +17,93 @@ export enum RELATIVE_DIRECTION {
     RIGHT,
 }
 
+export enum ATTRIBUTE {
+    STRENGTH,
+    ENDURANCE,
+    VITALITY,
+    AGILITY,
+    DEXTERITY,
+    SPEED,
+    LOGIC,
+    INTUITION,
+    PERCEPTION,
+    CHARISMA,
+    WILL,
+    LUCK,
+}
+
+export enum SKILL {
+    LONG_BLADE,
+    SHORT_BLADE,
+    BLUNT,
+    AXE,
+    POLEARM,
+    STAFF,
+    BOW,
+    CROSSBOW,
+    SHIELD,
+    UNARMORED,
+    CLOTH_ARMOR,
+    LEATHER_ARMOR,
+    CHAIN_ARMOR,
+    PLATE_ARMOR,
+    IMPOSITION,
+    INCANTATION,
+    SIGILRY,
+    SUPPLICATION,
+    MANIFESTATION,
+    CONVERSION,
+    ATAVISM,
+    ILLUSION,
+    BENEDICTION,
+    MALEDICTION,
+    ENHANCEMENT,
+    RESTORATION,
+    ALTERATION,
+    RECONFIGURATION,
+    REORIENTATION,
+    CONJURATION,
+    SUMMONING,
+    BINDING,
+    REPUDIATION,
+    PROPHECY,
+    ALCHEMY,
+    HERBALISM,
+    BUTCHERING,
+    PROSPECTING,
+    SMELTING,
+    SMITHING,
+    ENCHANTING,
+}
+
+export enum RESOURCE {
+    FLESH,
+    BLOOD,
+    BONE,
+    SOUL,
+    STAMINA,
+    MANA,
+}
+
+export enum EQUIPMENT_SLOT {
+    WEAPON = 0,
+    SHIELD = 1,
+    HELMET = 2,
+    CHEST_ARMOR = 3,
+    LEG_ARMOR = 4,
+    BOOTS = 5,
+    GLOVES = 6,
+    BELT = 7,
+    NECKLACE = 8,
+    RING = 9,
+    RING_ALT = 10,
+}
+
 export interface ArmorData {
     coverage: number;
     resilience: number;
     armor: number;
-    slot: 'WEAPON' | 'SHIELD' | 'HELMET' | 'CHEST_ARMOR' | 'LEG_ARMOR' | 'BOOTS' | 'GLOVES' | 'BELT' | 'NECKLACE' | 'RING' | 'RING_ALT';
+    slot: keyof typeof EQUIPMENT_SLOT;
 }
 
 export interface WeaponData {
@@ -45,6 +127,51 @@ export interface Item {
 }
 
 export type Inventory = Item[];
+
+export type CharacterEquipment = Record<keyof typeof EQUIPMENT_SLOT, Item>;
+
+export type CharacterAttributes = Record<keyof typeof ATTRIBUTE, number>;
+
+export type CharacterSkills = Record<keyof typeof SKILL, number>;
+
+export interface CharacterTrait {
+    id: string;
+    buyable: boolean;
+    cost: number;
+    name: string;
+    description: string;
+    effects: any[];
+}
+
+export interface CharacterRace {
+    raceID: string;
+    name: string;
+    description: string;
+    playable: boolean;
+    bodySize: string;
+    baseAttributes: CharacterAttributes;
+    traits: CharacterTrait[];
+}
+
+type Pools = Record<keyof typeof RESOURCE, { quantity: number, capacity: number }>
+
+export interface CharacterStatus {
+    action_points: number;
+    max_action_points: number;
+    action_point_recovery: number;
+    pools: Pools;
+}
+
+export interface CharacterSheet {
+    equipment: CharacterEquipment;
+    race: CharacterRace;
+    traits: CharacterTrait[];
+    skills: CharacterSkills;
+    status: CharacterStatus;
+    allocatedAttributes: CharacterAttributes;
+    essence: number;
+    exp: number;
+}
 
 export interface Entity {
     id: string;
