2f82716574cffcaeec2842efabf1bd64fc6f9b3d
phi <git@proffitt.io>
2020-05-25 13:24:42 -0500

    small fix to look event when direction is 0

--- src/world/event/look_event.ts
+++ src/world/event/look_event.ts
@@ -6,7 +6,7 @@ export class LookEvent {
     public type: EVENT_TYPE.LOOK = EVENT_TYPE.LOOK;
     constructor(private entity: Entity, private direction?: DIRECTION) { }
     public getClientJSON(viewer: Entity) {
-        const vec = (this.direction) ? (directionVectors[this.direction]) : { 'x': 0, 'y': 0 };
+        const vec = (this.direction !== undefined) ? (directionVectors[this.direction]) : { 'x': 0, 'y': 0 };
         const loc = this.entity.location.translate(vec.x, vec.y);
         const ents = loc.getEntitiesAt().map((ent) => ent.getComponent('name')).filter((name) => name !== undefined) as string[];
         let message;
