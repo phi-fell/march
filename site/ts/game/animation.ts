fef7f11c1f76f956d20fa9f41e9694f46b30f8a3
phi <git@proffitt.io>
2020-05-26 16:23:20 -0500

    origin anchor angle

--- site/ts/game/animation.ts
+++ site/ts/game/animation.ts
@@ -14,7 +14,7 @@ export class Animation {
     private scale: { x: number, y: number } = { 'x': 1, 'y': 1 };
     private angle: number = 0;
     private frames: HTMLCanvasElement[] = [];
-    private origin_anchor = { 'x': 0, 'y': 0 };
+    private origin_anchor = { 'x': 0, 'y': 0, 'angle': 0 };
     private anchors: { [id: string]: Anchor | undefined } = {};
     constructor(id: string) {
         this.image.onload = () => {
@@ -26,6 +26,9 @@ export class Animation {
                 this.offset = json.offset;
                 this.scale = json.scale;
                 if (json.origin_anchor) {
+                    if (json.origin_anchor.angle === undefined) {
+                        json.origin_anchor.angle = 0;
+                    }
                     this.origin_anchor = json.origin_anchor;
                 }
                 if (json.angle !== undefined) {
@@ -139,6 +142,7 @@ export class Animation {
                                 context.translate(anchor[frame].x, anchor[frame].y)
                                 context.rotate(anchor[frame].rot);
                                 context.translate(-animation.origin_anchor.x, -animation.origin_anchor.y);
+                                context.rotate(animation.origin_anchor.angle);
                                 animation.draw(context, time);
                                 context.pop();
                             }
